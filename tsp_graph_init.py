317270aadffb742a521fab3a55b197639687d8c7
Maeuva <136311737+Maeuva@users.noreply.github.com>
2025-03-17 14:24:00 +0100

    Update tsp_graph_init.py

--- tsp_graph_init.py
+++ tsp_graph_init.py
@@ -10,13 +10,14 @@ LARGEUR = 800
 HAUTEUR = 600
 
 class Lieu :
-    def __init__(self, x=None, y=None) :
-        if (x is None) :
-            x = np.random.rand()*LARGEUR
-        if (y is None) :
-            y = np.random.rand()*HAUTEUR
+    def __init__(self, x=None, y=None, nom=None):
+        if (x is None):
+            x = np.random.rand() * LARGEUR
+        if (y is None):
+            y = np.random.rand() * HAUTEUR
         self.x = float(x)
         self.y = float(y)
+        self.nom = nom
 
     def get_x(self) :
         return self.x
@@ -36,76 +37,6 @@ class Lieu :
     def __repr__(self):
         return "(" + str(round(self.x,2)) + "," + str(round(self.y,2)) + ")"
         
-
-class Graph :
-  
-    def __init__(self, NB_LIEUX, liste_lieux = None):
-        if liste_lieux is None :
-            self.liste_lieux = [ Lieu() for i in range(NB_LIEUX)]
-        else :            
-            self.liste_lieux = liste_lieux
-    
-    def get_liste_lieux(self):
-        return self.liste_lieux
-
-    def set_liste_lieux(self, liste_lieux):
-        self.liste_lieux = liste_lieux
-
-    def calcul_matrice_cout_od(self):
-        matrice_od = []
-        for i in range(NB_LIEUX) :
-            row = []
-            for j in range(NB_LIEUX) : 
-                row.append(set_liste_lieux[i].calcul_distance(set_liste_lieux[j]))
-            matrice_od.append(row)
-        return matrice_od 
-
-    def plus_proche_voisin(self):
-        liste_voisins = []
-        for i in range(NB_LIEUX):
-            min_dist = self.calcul_matrice_cout_od()[i][0]
-            numero_lieu = 0
-            for j in range(1,NB_LIEUX):
-                valeur = self.calcul_matrice_cout_od()[i][j]
-                if (min_dist > valeur and valeur != 0) or min_dist == 0:
-                    min_dist = self.calcul_matrice_cout_od()[i][j]
-                    numero_lieux = j
-            liste_voisins.append(self.liste_lieux[numero_lieux])
-        return liste_voisins
-
-
-    def calcul_distance_route(self, liste_coordonnees):
-        lieu = liste_coordonnees[0]
-        somme_distance = 0
-        for i in range(1, NB_LIEUX):
-            somme_distance += Lieu.calcul_distance(lieu, liste_coordonnees[i])
-            print(Lieu.calcul_distance(lieu, liste_coordonnees[i]))
-            lieu = liste_coordonnees[i]
-        somme_distance += Lieu.calcul_distance(liste_coordonnees[0], liste_coordonnees[NB_LIEUX-1])
-        return somme_distance
-    
-    def charger_graph(self, fichier_lieux):
-        """Charge la liste des lieux à partir d'un fichier CSV."""
-        self.liste_lieux = []  # Réinitialisation
-        try:
-            with open(fichier_lieux, newline='', encoding='utf-8') as csvfile:
-                reader = csv.reader(csvfile)
-                next(reader)  # Ignorer l'en-tête si présent
-                for row in reader:
-                    x, y = map(float, row)  # Convertir les coordonnées en flottants
-                    self.liste_lieux.append((x, y))
-        except Exception as e:
-            print(f"Erreur lors du chargement du fichier des lieux : {e}")
-    
-    def charger_matrice_od(self, fichier_matrice):
-        """Charge la matrice de distances à partir d'un fichier CSV."""
-        try:
-            with open(fichier_matrice, newline='', encoding='utf-8') as csvfile:
-                reader = csv.reader(csvfile)
-                self.matrice_od = np.array([list(map(float, row)) for row in reader])
-        except Exception as e:
-            print(f"Erreur lors du chargement de la matrice de distances : {e}")
-
 class Route :
     #ordre vaut None par defaut
     def __init__(self, ordre = None):
@@ -127,6 +58,119 @@ class Route :
 
     def set_ordre(self, ordre):
         self.ordre = ordre
+
+    def __eq__(self, autre_route):
+        return self.ordre == autre_route.ordre
+
+    def __neq__(self, autre_route):
+        return self.ordre != autre_route.ordre
+
+    def __lt__(self, autre_route):
+        return self.distance < autre_route.distance
+
+    def __gt__(self, autre_route):
+        return self.distance > autre_route.distance
+
+    def __le__(self, autre_route):
+        return self.distance <= autre_route.distance
+
+    def __ge__(self, autre_route):
+        return self.distance >= autre_route.distance
+
+
+class Graph :
+  
+    def __init__(self, NB_LIEUX, liste_lieux = None):
+        if liste_lieux is None :
+            self.liste_lieux = [ Lieu() for i in range(NB_LIEUX)]
+        else :            
+            self.liste_lieux = liste_lieux
+        self.matrice_cout_od = self.calcul_matrice_cout_od()
+        
+    def get_liste_lieux(self):
+        return self.liste_lieux
+
+    def set_liste_lieux(self, liste_lieux):
+        self.liste_lieux = liste_lieux
+
+    def calcul_matrice_cout_od(self):
+        matrice_od = []
+        for i in range(NB_LIEUX) :
+            row = []
+            for j in range(NB_LIEUX) : 
+                row.append(set_liste_lieux[i].calcul_distance(set_liste_lieux[j]))
+            matrice_od.append(row)
+        return matrice_od 
+
+    def calcul_distance_route(self, ordre):
+        lieu_actuel = self.liste_lieux[ordre[0]]
+        somme_distance = 0
+        for i in range(1, len(ordre)):
+            somme_distance += Lieu.calcul_distance(self.liste_lieux[lieu_actuel.nom],
+                                                   self.liste_lieux[ordre[i]])
+            lieu_actuel = self.liste_lieux[ordre[i]]
+        return somme_distance
+    
+    def plus_proche_voisin(self, lieu_depart, lieux_voisins):
+
+        ligne = self.matrice_cout_od[lieu_depart.nom]
+        indices_restants = [l.nom for l in lieux_voisins]
+
+        # Filtrer la ligne pour ne garder que les colonnes des lieux restants
+        ligne_filtree = [ligne[i] for i in indices_restants]
+        min_value = {"dist": ligne_filtree[0], "name": None}
+        for i, dist in enumerate(ligne_filtree):
+            if i != lieu_depart:
+                if dist < min_value['dist'] or min_value['dist'] == 0:
+                    min_value["dist"], min_value['name'] = dist, lieux_voisins[i].nom
+
+        if min_value['name'] is None and lieux_voisins:
+            min_value['name'] = lieux_voisins[0].nom
+
+        lieu_pp = next((lieu for lieu in lieux_voisins if lieu.nom == min_value["name"]), None)
+        return lieu_pp
+
+    def route_ppv(self, nom_lieu_depart) -> Route():
+        lieu_actuel = self.liste_lieux[0]
+        ordre = [nom_lieu_depart]
+        lieux_restants = self.liste_lieux.copy()
+        for lieu in lieux_restants:
+            if lieu.nom == nom_lieu_depart:
+                lieux_restants.remove(lieu)
+        while lieux_restants:
+            lieu_suivant = self.plus_proche_voisin(lieu_actuel, lieux_restants)
+            ordre.append(lieu_suivant.nom)
+            lieux_restants.remove(lieu_suivant)
+            lieu_actuel = lieu_suivant
+        ordre.append(nom_lieu_depart)
+        print("La route est " + str(ordre) + " et sa distance est de " + str(self.calcul_distance_route(ordre)))
+        return Route(ordre, self.calcul_distance_route(ordre))
+
+    
+        def charger_graph(self, fichier_lieux):
+        """Charge la liste des lieux à partir d'un fichier CSV."""
+        self.liste_lieux = []  # Réinitialisation
+        try:
+            with open(fichier_lieux, newline='', encoding='utf-8') as csvfile:
+                reader = csv.reader(csvfile)
+                next(reader)  # Ignorer l'en-tête si présent
+                for i, row in enumerate(reader):
+                    x, y = map(float, row)  # Convertir les coordonnées en flottants
+                    self.liste_lieux.append(Lieu(x=x, y=y, nom=i))
+            return self.liste_lieux
+        except Exception as e:
+            print(f"Erreur lors du chargement du fichier des lieux : {e}")
+    
+    def charger_matrice_od(self, fichier_matrice):
+        """Charge la matrice de distances à partir d'un fichier CSV."""
+        try:
+            with open(fichier_matrice, newline='', encoding='utf-8') as csvfile:
+                reader = csv.reader(csvfile)
+                self.matrice_od = np.array([list(map(float, row)) for row in reader])
+        except Exception as e:
+            print(f"Erreur lors du chargement de la matrice de distances : {e}")
+
+
 
 
 class Affichage :
